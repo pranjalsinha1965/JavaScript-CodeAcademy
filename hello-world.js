7b285191c32f5a3ff5c7a35c918e017a34b538d4
Pranjal Sinha <87580655+pranjalsinha1965@users.noreply.github.com>
2024-01-26 11:13:38 +0530

    Create hello-world.js

---
+++ hello-world.js
@@ -0,0 +1,68 @@
+//Exercise 2 - Console
+// console.log(5);
+
+
+//Exercise 3 - Comments
+//This is a single line comment 
+
+/*
+This is a multi line comment
+console.log(10)
+console.log(11)
+console.log(12)
+*/
+
+// let x = 10; 
+//Uncomment this to experiance a error
+// le y = 20;
+
+
+//Exercise 4 - Data Types
+// 'Javascript' - String
+// 15043 - Number
+// 53.21 - Number
+// False - Boolean
+// null - Null
+// undefined - Undefined
+// console - Object
+
+//Single or Double Quotes?
+
+//Error: console.log('We'll get started with doing math');
+//No Error: console.log("We'll get started with doing math");
+
+//Error: console.log("No, He said: "I do not like Math"");
+//No Error: console.log('No, He said: "I do not like Math"');
+
+
+//Exercise 5 - Arithmetic Operators
+// console.log(3 + 4);
+// console.log('3+4');
+
+//Exercse 6 - String Concatenation
+//console.log("Hello World");
+//console.log("Hello " + "World");
+
+// let name = "Emi";
+// console.log("Hello " + name);
+// console.log(name + " is great");
+
+//Exercise 7 (Properties) and 8 (Methods)
+
+//Property
+console.log("String".length);
+
+//Method
+console.log("String".toUpperCase());
+console.log("String".startsWith("S"));
+
+//House Analogy
+// Property -> # of Windows
+// Method -> Closing a window
+
+console.log(Math.floor(48.6))
+
+
+
+
+
