b8af085958ad1877c2951f1fc80d65c1c1460320
Pranjal Sinha <87580655+pranjalsinha1965@users.noreply.github.com>
2024-01-26 11:15:14 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,84 @@
+// Exercise 2 - Var
+var myName = 'David'
+
+// Exercise 3 - Let
+let myOtherName = 'Nik'
+
+// Exercise 4 - Const
+const birthCountry = 'Italy'
+
+let score = 0;
+let new_score1 = 1;
+let new_score2 = 5;
+
+// Exercise 5 - Math Assignment Operators
+
+let baz = true;
+
+//Boolean + Number
+baz += 1;
+// console.log(baz);
+
+
+let foo = 'foo';
+
+//String + Boolean
+foo += false;
+// console.log(foo)
+
+//Number + String
+foo += 7;
+// console.log(foo);
+
+
+// Exercise 6 - Increment/Decrement Operators
+// Prefix and Postfix
+
+// let num = 10;
+// num++;
+// console.log(num);
+// ++num;
+// console.log(num);
+
+
+let prefix = 10;
+let postfix = 10;
+
+let newPrefix = ++prefix;
+// console.log(newPrefix);
+
+let newPostfix = postfix++;
+// console.log(newPostfix);
+
+
+//Exercise 7 - String Concat with Variables
+//Adding Spaces
+
+let name = "Emi";
+// console.log("Hello " + name);
+// console.log("Hello" + " " + name);
+// console.log(name + " is great");
+
+
+//Exercise 9 - typeof Operator
+
+"hello".length
+
+typeof "hello"
+
+console.log("hello")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
